747ac171cec25bd028dd8ea846930a56e9ebe2db
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-09-20 12:17:37 +0000

    Wed, Sep 20, 2023, 7:17 AM -05:00

--- population/population.c
+++ population/population.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    cout <<'Ingrese el v'
     // TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
