12a2b555afc62d1662f2acbb3a5ae0aa96381225
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-09-24 23:20:13 +0000

    Sun, Sep 24, 2023, 6:20 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,36 +6,36 @@
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)   //to check if command line argument is not equal to 2
+    if (argc != 2)
     {
-        printf("Usage: ./caesar key\n"); //print  "Usage: ./caesar key if not equal to 2"
+        printf("Usage: ./caesar key\n");
         return 1;
     }
-    int arg_length = strlen(argv[1]);    //to get the length of string in command line argument
-    for (int i = 0; i < arg_length; i++)  //iterate through the length of the argv[1]
+    int arg_length = strlen(argv[1]);
+    for (int i = 0; i < arg_length; i++)
     {
-        if (!isdigit(argv[1][i]))  //to check if character  in argv[1] is not digit
+        if (!isdigit(argv[1][i]))
         {
-            printf("Usage: ./caesar key");  //if not print this //
+            printf("Usage: ./caesar key");
             return 1;
         }
     }
 
-    int key = atoi(argv[1]); //convert string to integer
-    string plaintext = get_string("plaintext: ");  //get plaintext to encipher  from user
-    printf("ciphertext: ");     //print the encipher text here
-    int plaintext_length = strlen(plaintext);  //to check the length of plaintext
-    for (int i = 0; i < plaintext_length; i++)  //iterate through the length of plaintext
+    int key = atoi(argv[1]);
+    string plaintext = get_string("plaintext: ");
+    printf("ciphertext: ");
+    int plaintext_length = strlen(plaintext);
+    for (int i = 0; i < plaintext_length; i++)
     {
-        if (isupper(plaintext[i]))   // if plaintext character is Upper case.......encipher it with this calculation
+        if (isupper(plaintext[i]))
         {
-            printf("%c", (((plaintext[i] - 65) + key) % 26) + 65);   // encipher the text with the calculation
+            printf("%c", (((plaintext[i] - 65) + key) % 26) + 65);
         }
-        else if (islower(plaintext[i]))  //if plaintext character is lower case.....encipher with the below calculation
+        else if (islower(plaintext[i]))
         {
-            printf("%c", (((plaintext[i] - 97) + key) % 26) + 97);  //enciper the text with the calculation
+            printf("%c", (((plaintext[i] - 97) + key) % 26) + 97);
         }
-        else                       //if not Uppercase or Lowercase, print the character like that
+        else
         {
             printf("%c", plaintext[i]);
         }
