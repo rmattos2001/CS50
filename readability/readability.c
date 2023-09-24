4fb6d94e08e79f5ddc56e25c1eafc63566763d60
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-09-24 20:52:07 +0000

    Sun, Sep 24, 2023, 3:52 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,29 +1,57 @@
 #include <cs50.h>
+// index = 0.0588 * L - 0.296 * S - 15.8
+
+#include <cs50.h>
 #include <stdio.h>
-using namespace std;
+#include <string.h>
+#include <math.h>
 
-int count_sentences(char[]);
-int count_letters(char[]);
+int main(void) // int argc, string argv[]
+{
+    // get user to analyze
+    string string = get_string("What is the string to analyze? \n");
 
-int main(void){
-    char cadena[200];
-    int z, espace=0,letters=0;
+    int letterCount = 0, wordCount = 0, sentenceCount = 0;
 
-    cout<<"Enter String :\n";
-    cin.getline(cadena,200);
-    cout<<"The entered text string contains "<<count_sentences(cadena)<<" works.";
-    //cout<<"The entered text string contains "<<count_letters(cadena)<<" Letters.";
-    return 0;
-}
-
-int count_sentences(char cad[]){
-    int i,words = 0;
-    for(i=1; cad[i]!='\0'; ++i){
-        if(cad[i-1]==' '&&isalpha(cad[i])){
-            ++words;
+    // count letters, words, and sentences
+    // assume a..z and A..Z is a letter
+    for (int i = 0, stringLength = strlen(string); i < stringLength + 1; i++)
+    {
+        if ((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= 'A' && string[i] <= 'Z'))
+        {
+            letterCount++;
         }
+        // any chars seperated by space is a word, check end of string count last word
+        if (string[i] == ' ' || string[i] == '\0')
+        {
+            wordCount++;
+
+        }
+        // when you see a . ! or ? count as sentence
+        if (string[i] == '!' || string[i] == '.'|| string[i] == '?')
+        {
+            sentenceCount++;
+        }
+
     }
 
-    if(isalpha(cad[0])) ++words;
-    return words;
+    // calculate the grade reading index
+    float averageWordsPer100 = (100 / (float) wordCount) * (float) letterCount;
+    float averageSentencePer100 = (100 / (float) wordCount) * (float) sentenceCount;
+    int readingIndex = round(0.0588 * averageWordsPer100 - 0.296 * averageSentencePer100 - 15.8);
+
+    if (readingIndex < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (readingIndex > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", readingIndex);
+    }
+
+    return 0;
 }
