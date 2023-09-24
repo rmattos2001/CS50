ec324af2d97a52721baccb267628ca0d8b8103a1
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-09-24 23:12:24 +0000

    Sun, Sep 24, 2023, 6:12 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,61 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    if (argc > 2 || argc < 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 
+    int k = atoi(argv[1]) % 26;
+
+    if (k <= 0)
+    {
+        printf("Usage ./caesar key\n");
+        return 1;
+    }
+
+    string plaintext = get_string("plaintext:  ");
+
+    int lenText = strlen(plaintext);
+
+    char ciphertext[lenText];
+
+    for (int i = 0; i < lenText; i++)
+    {
+        k = atoi(argv[1]) % 26;
+
+        if (isalpha(plaintext[i]))
+        {
+            if (islower(plaintext[i]))
+            {
+                if (plaintext[i] + k > 122)
+                {
+                    k = k - 26;
+                }
+            }
+
+            else
+            {
+                if (plaintext[i] + k > 90)
+                {
+                    k = k - 26;
+                }
+            }
+
+            ciphertext[i] = plaintext[i] + k;
+        }
+
+        else
+        {
+            ciphertext[i] = plaintext[i];
+        }
+    }
+
+    printf("ciphertext: %s\n", ciphertext);
 }
