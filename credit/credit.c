c5cb1fcb9242be8499ffeff53cf29d1b4b815c43
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-09-24 22:23:46 +0000

    Sun, Sep 24, 2023, 5:23 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,11 +12,11 @@ int main(void)
     int fDigit = 0;
     int sDigit = 0;
     int ultimoDigito;
-    while (numCartao != 0)
+    while (numCard != 0)
     {
         cont++;
         sDigit = ultimoDigito;
-        ultimoDigito = numCartao % 10;
+        ultimoDigito = numCard % 10;
         fDigit = ultimoDigito;
         if (cont % 2 == 0)
         {
@@ -35,18 +35,18 @@ int main(void)
         {
             addNSub = addNSub + ultimoDigito;
         }
-        numCartao = (numCartao - ultimoDigito) / 10;
+        numCard = (numCard - ultimoDigito) / 10;
     }
-    int somaTot = addSub + addNSub;
-    if (cont == 15 && fDigit == 3 && (sDigit == 4 || sDigit == 7) && somaTot % 10 == 0)
+    int addTot = addSub + addNSub;
+    if (cont == 15 && fDigit == 3 && (sDigit == 4 || sDigit == 7) && addTot % 10 == 0)
     {
         printf("AMEX\n");
     }
-    else if (cont == 16 && fDigit == 5 && sDigit >= 1 && sDigit <= 5 && somaTot % 10 == 0)
+    else if (cont == 16 && fDigit == 5 && sDigit >= 1 && sDigit <= 5 && addTot % 10 == 0)
     {
         printf("MASTERCARD\n");
     }
-    else if ((cont == 13 || cont == 16) && fDigit == 4 && somaTot % 10 == 0)
+    else if ((cont == 13 || cont == 16) && fDigit == 4 && addTot % 10 == 0)
     {
         printf("VISA\n");
     }
