16e0ec6df2de7155c30052fe1bbee838f6f5158c
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-09-29 15:16:03 +0000

    Fri, Sep 29, 2023, 10:16 AM -05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -10,8 +10,7 @@ typedef struct
 {
     string name;
     int votes;
-}
-candidate;
+} candidate;
 
 // Array of candidates
 candidate candidates[MAX];
@@ -80,7 +79,6 @@ bool vote(string name)
         }
     }
 
-
     if (!candidateSeek)
     {
         return false;
@@ -110,8 +108,8 @@ void print_winner(void)
             }
             else
             {
-        //Equality but one candidate has more votes than the tied candidates
-        //Clear the tied winning candidates and declare that candidate the winner.
+        /*Equality but one candidate has more votes than the tied candidates
+        Clear the tied winning candidates and declare that candidate the winner.*/
                 for (int j = 0; j < winners_count; j++)
                 {
                     winners[j] = winners[winners_count];
