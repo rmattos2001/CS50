dccb7ca4cc2b975333fcc9f75e8de85d1f5efc1f
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-02 02:40:41 +0000

    Sun, Oct 1, 2023, 9:40 PM -05:00

--- population/population.c
+++ population/population.c
@@ -3,13 +3,27 @@
 
 int main(void)
 {
-    // TODO: Prompt for start size
-    char a = "robert";
-    cout << a;
-    return 0;
-    // TODO: Prompt for end size
+    // Prompt for start size
+    int start_size;
+    do
+    {
+        start_size = get_int("Start size: ");
+    }
+    while (start_size < 9);
 
-    // TODO: Calculate number of years until we reach threshold
+    // Prompt for end size
+    int end_size;
+    do
+    {
+        end_size = get_int("End size: ");
+    }
+    while (end_size < start_size);
 
-    // TODO: Print number of years
+    // Calculate number of years until we reach threshold
+    // Big O(1)
+    int year = (int) floor(log( (double)end_size/ (double)start_size) / log(13.0/12.0));
+
+
+    // Print number of years
+    printf("Years: %i\n", year);
 }
