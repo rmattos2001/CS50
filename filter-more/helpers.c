259beceef794f766bbbe1a4a52df143e754277cd
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-07 01:40:17 +0000

    Fri, Oct 6, 2023, 8:40 PM -05:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -28,7 +28,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         // Instead of checking all pixel columns, we will only check the middle, like
         for (int x = 0, temp; x < width / 2; x++)
         {
-            // Troca o pixel na coluna x com o pixel na coluna inversa correspondente
+            // Swap the x column pixel with the corresponding reverse column pixel
             temp = image[y][x].rgbtBlue;
             image[y][x].rgbtBlue = image[y][width - 1 - x].rgbtBlue;
             image[y][width - 1 - x].rgbtBlue = temp;
@@ -48,6 +48,53 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            copy[y][x].rgbtBlue = image[y][x].rgbtBlue;
+            copy[y][x].rgbtGreen = image[y][x].rgbtGreen;
+            copy[y][x].rgbtRed = image[y][x].rgbtRed;
+        }
+    }
+
+    // Repete o processo para todos os pixeis
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            // Define variáveis para a soma de cada valor rgb, contador e raio do blur
+            int blueSum = 0, greenSum = 0, redSum = 0, c = 0;
+            int r = 1;
+
+            // Verifica cada pixel e soma seus valores rgb de acordo com o raio
+            // Ex: raio 1 a soma será feita para todas os pixeis até 1 de distância do pixel principal
+            for (int row = y - r; row <= y + r; row++)
+            {
+                for (int column = x - r; column <= x + r; column++)
+                {
+                    if ((row >= 0 && column >= 0) && (row <= height - 1 && column <= width - 1))
+                    {
+                        blueSum += copy[row][column].rgbtBlue;
+                        greenSum += copy[row][column].rgbtGreen;
+                        redSum += copy[row][column].rgbtRed;
+                        c++;
+                    }
+                }
+            }
+
+            // Tira a média da soma, de acordo com quantos pixels foram somados e arredondando
+            int averageBlue = round(blueSum * 1.0 / c);
+            int averageGreen = round(greenSum * 1.0 / c);
+            int averageRed = round(redSum * 1.0 / c);
+
+            // Muda os valores rgb do pixel para a média rgb dos pixels à sua volta
+            image[y][x].rgbtBlue = averageBlue;
+            image[y][x].rgbtGreen = averageGreen;
+            image[y][x].rgbtRed = averageRed;
+        }
+    }
     return;
 }
 
