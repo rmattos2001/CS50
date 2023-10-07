0ed95c2d24abf9ecf6c94251abb90b3310ad4e40
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-07 00:53:10 +0000

    Fri, Oct 6, 2023, 7:53 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,6 +25,40 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            // Declare rgb variables for the pixel
+            int originalBlue = image[y][x].rgbtBlue;
+            int originalGreen = image[y][x].rgbtGreen;
+            int originalRed = image[y][x].rgbtRed;
+
+            // Calcula as versões sépia do pixel
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+
+            // Limita a versão sépia a um máximo de 255
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            // Muda os valores rgb do pixel para suas versões sépia
+            image[y][x].rgbtBlue = sepiaBlue;
+            image[y][x].rgbtGreen = sepiaGreen;
+            image[y][x].rgbtRed = sepiaRed;
+        }
+    }
     return;
 }
 
