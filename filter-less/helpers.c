03f7732c6dbc092dc174d48a3196d91bdfbf2b92
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-07 00:54:43 +0000

    Fri, Oct 6, 2023, 7:54 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -34,7 +34,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int originalGreen = image[y][x].rgbtGreen;
             int originalRed = image[y][x].rgbtRed;
 
-            // Calcula as versões sépia do pixel
+            // Calculates sepia pixel versions.
             int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
             int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
             int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
