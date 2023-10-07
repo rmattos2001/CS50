e0b53a808f6bde5464a9395d3ca622fae214684f
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-07 01:37:36 +0000

    Fri, Oct 6, 2023, 8:37 PM -05:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -23,6 +23,26 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int y = 0; y < height; y++)
+    {
+        // Em vez de verificar todas as colunas de pixel, verificaremos apenas metade, pois ao
+        // Trocar metade dos pixeis a outra metade também vai ser trocada
+        for (int x = 0, temp; x < width / 2; x++)
+        {
+            // Troca o pixel na coluna x com o pixel na coluna inversa correspondente
+            temp = image[y][x].rgbtBlue;
+            image[y][x].rgbtBlue = image[y][width - 1 - x].rgbtBlue;
+            image[y][width - 1 - x].rgbtBlue = temp;
+
+            temp = image[y][x].rgbtGreen;
+            image[y][x].rgbtGreen = image[y][width - 1 - x].rgbtGreen;
+            image[y][width - 1 - x].rgbtGreen = temp;
+
+            temp = image[y][x].rgbtRed;
+            image[y][x].rgbtRed = image[y][width - 1 - x].rgbtRed;
+            image[y][width - 1 - x].rgbtRed = temp;
+        }
+    }
     return;
 }
 
