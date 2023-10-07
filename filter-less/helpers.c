a01d9ccdd2cf8d9aae6d34bf0467c30cab33b38e
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-07 00:50:10 +0000

    Fri, Oct 6, 2023, 7:50 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,6 +4,21 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            // Soma os valores rgb do pixel, calcula a média dessa soma e a arredonda
+            int sum = image[y][x].rgbtBlue + image[y][x].rgbtGreen + image[y][x].rgbtRed;
+            int average = round(sum / 3.0);
+
+            // Muda os valores rgb do pixel para serem iguais a média
+            image[y][x].rgbtBlue = average;
+            image[y][x].rgbtGreen = average;
+            image[y][x].rgbtRed = average;
+        }
+    }
+    
     return;
 }
 
