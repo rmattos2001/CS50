fb40086c56b17d75c6c5848d0238d153391bf2d6
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-07 00:12:35 +0000

    Fri, Oct 6, 2023, 7:12 PM -05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -6,17 +6,18 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     // By checking every line column of the image (all pixels)
     for (int i = 0; i < height; i++)
     {
-        for (int column = 0; column < width; column++)
+        for (int j = 0; j < width; j++)
         {
+            // i=line , j=column
             // If the values for red, green and blue, in the pixel (image[line[column]])
             // are all '0', than the pixel is black and we should change its color
-            if (image[i][column].rgbtBlue == 0
-                && image[i][column].rgbtGreen == 0
-                && image[i][column].rgbtRed == 0)
+            if (image[i][j].rgbtBlue == 0
+                && image[i][j].rgbtGreen == 0
+                && image[i][j].rgbtRed == 0)
             {
-                image[i][column].rgbtBlue = 66;
-                image[i][column].rgbtGreen = 94;
-                image[i][column].rgbtRed = 133;
+                image[i][j].rgbtBlue = 66;
+                image[i][j].rgbtGreen = 94;
+                image[i][j].rgbtRed = 133;
             }
         }
     }
