dd91b77330ce788b18be9feb84a22ca3bce2b924
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-13 20:50:36 +0000

    Fri, Oct 13, 2023, 3:50 PM -05:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,6 +52,8 @@ person *create_family(int generations)
         new_person->parents[0] = parent0;
         new_person->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        new_person->alleles[0] = new_person->parents[0]->alleles[rand() % 2];
+        new_person->alleles[1] = new_person->parents[1]->alleles[rand() % 2];
 
     }
 
@@ -59,9 +61,12 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
+        
         // TODO: Randomly assign alleles
-
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
