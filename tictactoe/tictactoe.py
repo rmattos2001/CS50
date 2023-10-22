72ac96c7b5fd1cc648e4c15ff20e2509f46c8472
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-22 06:51:11 +0000

    Sun, Oct 22, 2023, 1:51 AM -05:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -66,21 +66,21 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    # Check rows
+    # rows
     if all(i == board[0][0] for i in board[0]):
         return board[0][0]
     elif all(i == board[1][0] for i in board[1]):
         return board[1][0]
     elif all(i == board[2][0] for i in board[2]):
         return board[2][0]
-    # Check columns
+    # columns
     elif board[0][0] == board[1][0] and board[1][0] == board[2][0]:
         return board[0][0]
     elif board[0][1] == board[1][1] and board[1][1] == board[2][1]:
         return board[0][1]
     elif board[0][2] == board[1][2] and board[1][2] == board[2][2]:
         return board[0][2]
-    # Check diagonals
+    # diagonals
     elif board[0][0] == board[1][1] and board[1][1] == board[2][2]:
         return board[0][0]
     elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
@@ -101,11 +101,11 @@ def terminal(board):
     #not any(EMPTY in sublist for sublist in board
     #and winner(board) is None else False
 
-
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
+    # Reviews how to make the exception when a non-terminal board is received.
     if terminal(board):
         if winner(board) == X:
             return 1
@@ -113,11 +113,51 @@ def utility(board):
             return -1
         else:
             return 0
-    # Check how to handle exception when a non terminal board is received.
-
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    else:
+        if player(board) == X:
+            value, move = max_value(board)
+            return move
+        else:
+            value, move = min_value(board)
+            return move
+
+def max_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = float('-inf')
+    move = None
+    for action in actions(board):
+        # v = max(v, min_value(result(board, action)))
+        aux, act = min_value(result(board, action))
+        if aux > v:
+            v = aux
+            move = action
+            if v == 1:
+                return v, move
+
+    return v, move
+
+def min_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = float('inf')
+    move = None
+    for action in actions(board):
+        # v = max(v, min_value(result(board, action)))
+        aux, act = max_value(result(board, action))
+        if aux < v:
+            v = aux
+            move = action
+            if v == -1:
+                return v, move
+
+    return v, move
