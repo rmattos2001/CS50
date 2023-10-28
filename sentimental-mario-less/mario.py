82939a8ee00568e6dafe7d6c572da4bfc3a19f72
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-28 12:23:48 +0000

    Sat, Oct 28, 2023, 7:23 AM -05:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,12 @@
 # TODO
+# Pergunta ao usuário qual a altura da semi-pirâmide, (entre 1 e 8)
+while True:
+    height = get_int('Height: ')
+
+    if 1 <= height <= 8:
+        break
+
+# Mostra na tela os blocos da pirâmide
+for c in range(height):
+    c += 1
+    print(' ' * (height - c) + '#' * c)
