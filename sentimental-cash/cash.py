ece18a9d89c7daafd45a8addaf7ece56c54eb160
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-28 12:53:54 +0000

    Sat, Oct 28, 2023, 7:53 AM -05:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,50 +1,33 @@
 # TODO
 from cs50 import get_float
 
-# Declares our change and our minimum coins quantity needed globally.
-change_owed_in_dollars = min_coins_needed = 0
+while True:
+    cents = get_float("Change owed: ")
+    if cents > 0:
+        break
 
+cents = round(cents * 100)
 
-def main() -> None:
-    global change_owed_in_dollars, min_coins_needed
+count = 0
 
-    # Makes it only accepts positive values.
-    while True:
-        # Asks the change owed.
-        change_owed_in_dollars = get_float('Change owed: ')
-        if change_owed_in_dollars > 0:
-            break
+# Number of quarters
+while cents >= 25:
+    cents = cents - 25
+    count += 1
 
-    # For each type of coin calls the function.
-    for coin_type in [25, 10, 5, 1]:
-        calculate_min_coins_needed_for_the_change_owed(coin_type)
+# Number of dimes
+while cents >= 10:
+    cents = cents - 10
+    count += 1
 
-    # Shows on the screen the minimum amount of coins needed.
-    print(min_coins_needed)
+# Number of nickels
+while cents >= 5:
+    cents = cents - 5
+    count += 1
 
+# Numberr of penny
+while cents >= 1:
+    cents = cents - 1
+    count += 1
 
-# Receives the number of the coin (25, 10, 5, 1), calculate and updates;
-# the change_owed_in_dollars and the min_coins_needed accordingly.
-def calculate_min_coins_needed_for_the_change_owed(coin_type: int) -> None:
-    global change_owed_in_dollars, min_coins_needed
-
-    # Converts the change owed in dollars to cents ex: (5.75 -> 575);
-    # To avoid floating point imprecision, we will reconvert it later.
-    change_owed_in_cents = int(change_owed_in_dollars * 100)
-
-    # If it's possible to pay off some of the change owed with that coin.
-    if change_owed_in_cents / coin_type >= 1:
-        # Check how many coins can we give as change without exceed.
-        coins_quantity = int(change_owed_in_cents / coin_type)
-
-        # Sums the coins we gave.
-        min_coins_needed += coins_quantity
-
-        # Subtracts the change owed by the money we gave.
-        change_owed_in_cents -= (coins_quantity * coin_type)
-
-        # Reconvert the change owed to dollars.
-        change_owed_in_dollars = (change_owed_in_cents / 100)
-
-
-main()
+print(f"Total coins: {count}")
