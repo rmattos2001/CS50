ff322c9c2b7532ea71b4819fe6199c37e8c7adea
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-28 00:39:26 +0000

    Fri, Oct 27, 2023, 7:39 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -4,44 +4,37 @@ import csv
 import sys
 import random
 
-# Number of simuulations to run
+# Number of simluations to run
 N = 1000
 
 
 def main():
 
-    print('')
-
     # Ensure correct usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
-    # Read teams into memory from file
     teams = []
-    try:
-        file = open(sys.argv[1], 'r')
-    except:
-        print(f'\033[31mError opening the file "{sys.argv[1]}"\033[m\n')
-        sys.exit(1)
+    # TODO: Read teams into memory from file
+    f = open(f"{sys.argv[1]}", "r")
+    reader = csv.DictReader(f)
 
-    reader = csv.DictReader(file)
-    for pos, team in enumerate(reader):
-        teams.append(team)  # Append the team into teams list
-        teams[pos]['rating'] = int(teams[pos]['rating'])  # Converts rating of a team to int
+    for row in reader:
+        row["rating"] = int(row["rating"])
+        teams.append(row)
 
-    # TODO: Simulate N tournaments and keep track of win counts
     counts = {}
-    for c in range(N):
+    # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N):
         winner = simulate_tournament(teams)
-        if winner not in counts:
-            counts[winner] = 0
-        counts[winner] += 1
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
-
-    print('')
 
 
 def simulate_game(team1, team2):
@@ -68,14 +61,12 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    winners = teams
+    # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
 
-    # Will simulate rounds until theres only one winner
-    while len(winners) > 1:
-        winners = simulate_round(winners)
-
-    # Returns the name of that winner
-    return winners[0]['team']
+    # Only returning team (key), exclude (value).
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
