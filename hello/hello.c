4e17d0216be952f7e535682baf1364d4e5bc3d8e
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-10-29 12:16:54 +0000

    Sun, Oct 29, 2023, 7:16 AM -05:00

--- hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    string name = get_string("What is your name: "); //declare a string with 'name' variable
-
-    printf("hello, %s\n", name); //then print the name with %s
-}
