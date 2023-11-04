40899ac6d86225659671e974ec7b6a5aea0a1b89
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-11-04 23:11:31 +0000

    Sat, Nov 4, 2023, 6:11 PM -05:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,7 +1,6 @@
-SELECT name, rating
-FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE name = ("Chadwick Boseman")
-ORDER BY rating DESC LIMIT 5;
+SELECT title FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
+    JOIN ratings ON movies.id = ratings.movie_id
+    WHERE name = "Chadwick Boseman"
+    ORDER BY rating DESC LIMIT 5;
