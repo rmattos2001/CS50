c456f0bea081b6e6a788c6cee2f993e27a78a849
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-11-04 23:12:16 +0000

    Sat, Nov 4, 2023, 6:12 PM -05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,10 @@
-SELECT title
-FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE name = ("Helena Bonham Carter") AND ("Johnny Depp");
+SELECT title FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
+    WHERE people.name = "Helena Bonham Carter"
+    AND title IN (
+        SELECT title FROM movies
+            JOIN stars ON movies.id = stars.movie_id
+            JOIN people ON stars.person_id = people.id
+            WHERE people.name = "Johnny Depp"
+    );
