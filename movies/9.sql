dc13c0157d14fa38657159ae16d053882efc80d3
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-11-04 23:09:20 +0000

    Sat, Nov 4, 2023, 6:09 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT name FROM people
+SELECT DISTINCT(name) FROM people
     JOIN stars ON people.id = person_id
-    JOIN movies ON movie_id = movies.id
-    WHERE year = 2004
-    ORDER BY name ASC;
+    JOIN movies ON movies.id = movie_id
+    WHERE year = "2004"
+    ORDER BY birth;
