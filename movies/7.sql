ac972e79ccbebba832d3da98cad2582eb7faac8b
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-11-04 23:05:28 +0000

    Sat, Nov 4, 2023, 6:05 PM -05:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,6 +1,1 @@
-SELECT title, rating
-FROM movies
-JOIN ratings ON movie_id = movies.id
-WHERE year = "2010"
-GROUP BY title
-ORDER BY rating DESC;
+SELECT title, rating FROM movies JOIN ratings ON movie_id = movies.id WHERE year = "2010" GROUP BY title ORDER BY rating DESC;
--- movies/9.sql
+++ movies/9.sql
@@ -2,5 +2,5 @@ SELECT DISTINCT name
 FROM people
 JOIN stars ON people.id = person_id
 JOIN movies ON movie_id = movies.id
-WHERE year = 2004
+WHERE year = 2004 
 ORDER BY name ASC;
