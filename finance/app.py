217df4913d64cff40b6c737643c7f7380814cad3
rmattos2001 <40253744+rmattos2001@users.noreply.github.com>
2023-12-04 15:56:25 +0000

    Mon, Dec 4, 2023, 10:56 AM -05:00

--- finance/app.py
+++ finance/app.py
@@ -35,21 +35,77 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    stocks = db.execute("SELECT symbol, shares FROM stocks WHERE user_id = ?;", session["user_id"])
+    user_cash = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])[0]["cash"]
+
+    stocks_total_price = 0
+    for stock in stocks:
+        stocks_total_price += lookup(stock["symbol"])["price"] * stock["shares"]
+
+    return render_template("index.html", user_cash=user_cash, lookup=lookup, stocks=stocks, stocks_total_price=stocks_total_price)
 
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        symbol = request.form.get("symbol")
+        shares = request.form.get("shares")
+
+        # Search for the stock
+        stock = lookup(symbol)
+
+        # Ensure there is a stock with that symbol
+        if not stock:
+            return apology("invalid symbol", 400)
+
+        # Ensure shares was submitted
+        elif not shares:
+            return apology("missing shares", 400)
+
+        # Ensure shares is a positive number
+        elif not shares.isdigit() or int(shares) < 1:
+            return apology("invalid shares", 400)
+
+        user_cash = db.execute("SELECT cash FROM users WHERE id = ?;", session["user_id"])[0]["cash"]
+        shares_total_price = stock["price"] * int(shares)
+
+        # Ensure user has enough money to buy shares
+        if not user_cash > shares_total_price:
+            return apology("can't afford")
+
+        # Insert the new purchase into transactions, along with the stock symbol and price
+        db.execute("INSERT INTO transactions (user_id, symbol, price, shares, type) VALUES (?, ?, ?, ?, 'purchase');",
+                   session["user_id"], stock["symbol"], stock["price"], shares)
+
+        # Insert the stocks bought into stocks, along with the stock symbol
+        db.execute("INSERT INTO stocks (user_id, symbol, shares) VALUES (?, ?, ?);", session["user_id"], stock["symbol"], shares)
+
+        # Subtract user cash used into that purchase
+        db.execute("UPDATE users SET cash = ? WHERE id = ?;", user_cash - shares_total_price, session["user_id"])
+
+        # Success message
+        flash('Bought!')
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+
+    transactions = db.execute("SELECT symbol, shares, price, type, date FROM transactions WHERE user_id = ?;", session["user_id"])
+
+    return render_template("history.html", transactions=transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
